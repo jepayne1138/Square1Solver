de6fdc386e7200cab3d7f419373e51e5f09c5673
James Payne <jepayne1138@gmail.com>
2016-05-31 23:34:02 -0500

    First iteration of solution module
    Right now randomly picks one of the best evaluated solutions. Has some commented
    out code that would pick a possibility weighted (by eval) randomly, but this
    doesn't seems to work as high solutions are picked too rarely.
    
    This version does not solve the cube as it currently can get stuck in a loop
    of having no better solution and never backtracks.

--- solve_square.py
+++ solve_square.py
@@ -1,11 +1,22 @@
 from square1 import argparser
 from square1 import input_parser
+from square1 import solve_cube
 
 def main():
     args = argparser.parse()
     with open(args.input_file) as input_fileobj:
         cube = input_parser.read_input(input_fileobj)
-    print cube
+
+    try:
+        solution_steps = solve_cube.solve(cube, timeout=20)
+    except solve_cube.TimeoutException:
+        print 'Timeout reached with no solution found!'
+        return
+
+    # Print out the solution steps
+    print 'Solution found! - Steps:'
+    for steps in solution_steps:
+        print steps
 
 
 if __name__ == '__main__':
--- square1/solve_cube.py
+++ square1/solve_cube.py
@@ -13,33 +13,136 @@ choice of move to try.  In this way all moves have a possibility of being
 chosen, but increasing accurate choices are most likely.
 """
 import copy
+import time
+import math
+import random
+from pprint import pprint
+import bisect
 import cube_definition as cube_def
 import eval_function as eval_func
 
+MAX_TARGET = 0.95
+MIN_TARGET = 1 - MAX_TARGET
+WEIGHT_MULTIPLIER = 100
+
+class TimeoutException(Exception):
+    pass
+
+
+def _a(c1, c2):
+    return math.log(MAX_TARGET / MIN_TARGET) / (c1 - c2)
+
+
+def _b(c1, c2):
+    return (c2 * math.log(MAX_TARGET) - c1 * math.log(MIN_TARGET)) / (c2 - c1)
+
+
+def exponential_function(c1, c2):
+    """Returns an exponential function returns .95 for c1 and .05 for c2
+    Returns function in form of f(x) = e^(a*x+b)
+    """
+    return lambda x: WEIGHT_MULTIPLIER * math.exp(_a(c1, c2) * x + _b(c1, c2))
+
 
 def wedge_possibilities(cube):
-    cube_copy = copy.copy(cube)
+    # pprint(cube.wedges['top'])
     for top_wedge in cube.wedges[cube_def.TOP]:
         for bottom_wedge in cube.wedges[cube_def.BOTTOM]:
+            cube_copy = copy.deepcopy(cube)
             cube_copy.flip(top_wedge, bottom_wedge)
             yield (cube_copy, top_wedge, bottom_wedge)
     return
 
+
+def weighted_choice(weights, choices):
+    total = 0
+    cumulative_weights = []
+    for weight in weights:
+        total += weight
+        cumulative_weights.append(total)
+    rand_value = random.random() * total
+    selected_index = bisect.bisect(cumulative_weights, rand_value)
+    return choices[selected_index]
+
+
 def solve_step(cube):
     """Returns a new cube and the step that was selected to get there"""
     possibilites = {}
+    # pprint(cube.wedges['top'])
     # Iterate through all possible flips from current state
     for cube1, top1, bottom1 in wedge_possibilities(cube):
+        cube_copy = copy.deepcopy(cube)
+        try:
+            cube_copy.flip(top1, bottom1)
+            assert cube_copy == cube1
+        except AssertionError:
+            print 'cube1'
+            print cube1
+            print 'cube_copy'
+            print cube_copy
+            raise
+
+        assert id(cube) != id(cube1)
         for cube2, top2, bottom2 in wedge_possibilities(cube1):
+            assert id(cube1) != id(cube2)
+            assert cube != cube2
             eval_value = eval_func.evaluate1(cube2)
+            if eval_value == eval_func.EVAL_TARGET:
+                # If goal state, immediately return solution
+                # We don't care about returned cube as we don't need it again
+                return (None, top1, bottom1, top2, bottom2, True)
             if eval_value not in possibilites:
                 possibilites[eval_value] = []
             possibilites[eval_value].append(
                 (top1, bottom1, top2, bottom2)
             )
 
-    # Select one of the possibilities
-    sel_top1, sel_bottom1, sel_top2, sel_bottom2 = # Something here
+    # pprint(cube.wedges['top'])
+    # Select one of the possibilites
+    exp_func = exponential_function(
+        max(possibilites.keys()),
+        min(possibilites.keys()),
+    )
+    print 'Number of possibilites = {}'.format(sum(map(len, possibilites.values())))
+    print 'Max = {} : Min = {}'.format(
+        max(possibilites.keys()),
+        min(possibilites.keys()),
+    )
+    # weights = []
+    # choices = []
+    # for weight in possibilites.keys():
+    #     for choice in possibilites[weight]:
+    #         weights.append(weight)
+    #         choices.append(choice)
+    # sel_top1, sel_bottom1, sel_top2, sel_bottom2 = weighted_choice(
+    #     weights, choices
+    # )
+    sel_top1, sel_bottom1, sel_top2, sel_bottom2 = random.choice(
+        possibilites[max(possibilites.keys())]
+    )
     cube.flip(sel_top1, sel_bottom1)
     cube.flip(sel_top2, sel_bottom2)
-    return (cube, sel_top1, sel_bottom2, sel_top2, sel_bottom2)
+    assert eval_func.evaluate1(cube) == max(possibilites.keys())
+    return (cube, sel_top1, sel_bottom2, sel_top2, sel_bottom2, False)
+
+
+def solve(cube, timeout=None):
+    steps = []
+    cur_cube = cube
+    if timeout is not None:
+        start_time = time.time()
+    step_number = 0
+    while True:
+        step_number +=1
+        cur_cube, top1, bottom1, top2, bottom2, solved = solve_step(cur_cube)
+        steps.append(
+            (top1, bottom1, top2, bottom2)
+        )
+        print 'Step {: <3} = {}'.format(
+            step_number, eval_func.evaluate1(cur_cube)
+        )
+        if solved:
+            return steps
+        if timeout is not None:
+            if (time.time() - start_time) > timeout:
+                raise TimeoutException()
